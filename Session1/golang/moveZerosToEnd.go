bf15c1254e787d98c6750da7036ec95483859e17
abhey <ms.akaul@gmail.com>
2023-03-24 07:43:36 +0530

    wip

--- Session1/golang/moveZerosToEnd.go
+++ Session1/golang/moveZerosToEnd.go
@@ -2,15 +2,21 @@
 package golang
 
 // [0,1,0,3,12]
-func moveZeroes(arr []int) []int {
+
+func MoveZeros(arr []int) []int {
 	j := 0
 	for i := 0; i < len(arr); i++ {
 		if arr[i] != 0 {
-			temp := arr[i]
-			arr[i] = arr[j]
-			arr[j] = temp
+			arr = swap(arr, i, j)
 			j++
 		}
 	}
+	return arr
+}
+
+func swap(arr []int, i int, j int) []int {
+	temp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
 	return arr
 }
