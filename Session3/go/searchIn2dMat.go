97c1605b9c16f87d214b0cdb9c62d89bba2d6573
abhey <ms.akaul@gmail.com>
2023-04-05 22:48:07 +0530

    searhc in 2 d

--- Session3/go/searchIn2dMat.go
+++ Session3/go/searchIn2dMat.go
@@ -1,5 +1,27 @@
 package golang
 
-func searchIn2dMar(arr [][]int, el int) {
+func searchIn2dMar(arr [][]int, el int) bool {
+	for i := 0; i < len(arr); i++ {
+		innerArr := arr[i]
+		innerArrLastEl := innerArr[len(innerArr)-1]
+		if innerArrLastEl == el {
+			return true
+		}
+		innerArrFirstEl := innerArr[0]
+		if innerArrFirstEl < el && el < innerArrLastEl {
+			s, e := 0, len(innerArr)
+			for s <= e {
+				mid := (s + e) / 2
+				if mid == el {
+					return true
+				} else if mid > el {
+					s = mid + 1
+				} else {
+					e = mid - 1
+				}
 
+			}
+		}
+	}
+	return false
 }
